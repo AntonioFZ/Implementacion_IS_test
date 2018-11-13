ff0c1aa6b46ebcbcbf41772498ade679d2a6bed9
AntonioFZ <i62frzaa@uco.es>
2018-11-13 14:36:38 +0100

    Arreglo de escritura

--- T2/GENERADOR_CLAVES.cpp
+++ T2/GENERADOR_CLAVES.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <limits>
 
 using namespace std;
 
@@ -16,12 +17,42 @@ int main() {
 	//Introducir Datos
 	cout<<"Introduce clave para coordinador: ";
 	cin>>a;
+	while(1){
+		if(cin.fail()){
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(),'\n');
+			cout<<"Has introducido un caracter erroneo, vuelve a introducirlo: ";
+			cin>>a;
+		}
+		if(!cin.fail())
+		break;
+	}
 	cout<<"\n";
 	cout<<"Introduce clave para primer ayudante: ";
 	cin>>b;
+		while(1){
+		if(cin.fail()){
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(),'\n');
+			cout<<"Has introducido un caracter erroneo, vuelve a introducirlo: ";
+			cin>>b;
+		}
+		if(!cin.fail())
+		break;
+	}
 	cout<<"\n";
 	cout<<"Introduce clave para segundo ayudante: ";
 	cin>>c;
+	while(1){
+		if(cin.fail()){
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(),'\n');
+			cout<<"Has introducido un caracter erroneo, vuelve a introducirlo: ";
+			cin>>c;
+		}
+		if(!cin.fail())
+		break;
+	}
 	cout<<"\n";
 	
 	//Comprobacion Errores
