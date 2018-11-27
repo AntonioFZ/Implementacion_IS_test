c22e484c825f20ef1b058561619d39a463f5948b
Rafael Ayllon Gavilan <i72aygar@uco.es>
2018-11-27 10:50:58 +0100

    Añadido introducir alumno

--- T3/agenda.cpp
+++ T3/agenda.cpp
@@ -8,22 +8,103 @@ using namespace std;
 #include "menu.h"
 #include "struct.h"
 
-void Introducir_alumno(bool coordinador,int *n_alumnos){
-	//Limpia pantalla
-	system("clear");
-	//Lo que hace la función
-	cin>>Persona[1].DNI;
-	cin>>Persona[24].DNI;
-	system("clear");
-	cout<<"DNI.Persona 24 = "<<Persona[24].DNI<<endl;
-	sleep(1);
-	
-	//Añade al contador un alumno
-	(*n_alumnos)=(*n_alumnos)+1;
+//*************************INTRODUCIR ALUMNO INICIO****************************//
 
-	//Volver al MENU DE OPCIONES
-	Menu(coordinador);
+void IntroducirAlumno(bool coord, int *n_alumnos){
+
+bool lider;
+string newDNI;
+string newEmail;
+string esLiderSource;
+Persona[n_alumnos].lider = false;
+
+cout<< "Introduce el DNI \n"
+cin<< newDNI;
+checkDNI(newDNI);
+
+cout<< "Introduce el nombre \n"
+cin<< Persona[n_alumnos].nombre;
+
+cout<< "Introduce el apellidos \n"
+cin<< Persona[n_alumnos].apellidos;
+
+cout<< "Introduce el telefono \n"
+cin<< Persona[n_alumnos].telefono;
+
+cout<< "Introduce el email \n"
+cin<< newEmail;
+checkEmail(newEmail);
+
+cout<< "Introduce el domicilio \n"
+cin<< Persona[n_alumnos].domicilio;
+
+cout<< "Introduce el curso \n"
+cin<< Persona[n_alumnos].curso;
+
+cout<< "Introduce la fecha de nacimiento \n"
+cin<< Persona[n_alumnos].fecha;
+
+cout<< "Introduce el grupo al que pertenece \n"
+cin<< Persona[n_alumnos].grupo;
+
+cout<< "¿Es líder? Introduce 'Si', si no, introduce 'No' \n"
+cin<< esLiderSource;
+
+switch (esLiderSource){
+
+case "Si":
+checkLider(Persona[n_alumnos].grupo); 
+Persona[n_alumnos].lider = true;
+break;
+
+case "No":
+Persona[n_alumnos].lider = false;
+break;
 }
+
+n_alumnos++;
+Menu(coordinador);
+}
+
+
+void checkLider(int grupo){
+
+for(int i = 0; i<n_alumnos; i++){
+if( (Persona[i].grupo = grupo) && (Persona[i].lider) ){
+printf("El grupo al que pertenece el alumno ya tiene un líder")
+sleep(2);
+Menu(coordinador);
+}
+}
+}
+
+void checkDNI(int dni){
+
+for(int i = 0; i<n_alumnos; i++){
+if(Persona[i].DNI = dni){
+cout<<"Ya existe un alumno con ese DNI"
+sleep(2);
+Menu(coordinador);
+}else{
+Persona[n_alumnos].DNI = dni;
+}
+}
+}
+
+void checkEmail(string email){
+
+for(int i = 0; i<n_alumnos; i++){
+if(Persona[i].email = email){
+cout<<"Ya existe un alumno con ese email"
+sleep(2);
+Menu(coordinador);
+}else{
+Persona[n_alumnos].email = email;
+}
+}
+}
+
+//*************************INTRODUCIR ALUMNO FINAL****************************//
 
 void Mostrar_Agenda(bool coordinador){
 	system ("clear");
--- T3/agenda.h
+++ T3/agenda.h
@@ -5,6 +5,9 @@ using namespace std;
 
 
 void Introducir_alumno(bool coordinador,int *n_alumnos);
+void checkEmail(string email);
+void checkLider(int grupo);
+void checkDNI(int DNI);
 
 void Mostrar_Agenda(bool coordinador);
 
